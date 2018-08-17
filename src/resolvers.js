1dc2e5bb049f80f2ace753aefab8c26d0241cc96
Emerson Laurentino <emersonlaurentino@hotmail.com>
2018-08-17 15:06:15 -0300

    add(resolvers): order by in launches

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,10 +1,21 @@
 const axios = require('axios');
 const baseUrl = 'https://api.spacexdata.com/v2';
-const getUrl = endpoint => `${baseUrl}/${endpoint}`;
+const getUrl = endpoint => `${baseUrl}/${endpoint}?id=true`;
+const getData = ({ data }) => data;
 
 const resolvers = {
   Query: {
-    launches: () => axios.get(getUrl('launches')).then(({ data }) => data),
+    launches: (_, { input }) => {
+      const url = (() => {
+        if (input.orderBy) {
+          return `${getUrl('launches')}&order=${input.orderBy.toLowerCase()}`;
+        }
+
+        return getUrl('launches');
+      })();
+
+      return axios.get(url).then(getData);
+    },
   },
 };
 
